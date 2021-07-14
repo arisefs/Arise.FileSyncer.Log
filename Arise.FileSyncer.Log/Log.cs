dcdad91d97245c804f08c012abea1f44a1a67fa0
Csányi István <csanyistvan02@gmail.com>
2021-07-14 21:01:30 +0200

    Update C# format

--- Arise.FileSyncer.Log/Log.cs
+++ Arise.FileSyncer.Log/Log.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Arise.FileSyncer
 {
@@ -6,42 +6,18 @@ namespace Arise.FileSyncer
     {
         public delegate void DelegateLog(string message);
 
-        public static DelegateLog Error = DefaultError;
-        public static DelegateLog Warning = DefaultWarning;
-        public static DelegateLog Info = DefaultInfo;
-        public static DelegateLog Verbose = DefaultVerbose;
-        public static DelegateLog Debug = DefaultDebug;
-
-        private static void DefaultError(string message)
-        {
-            Console.WriteLine(Format("E: " + message));
-        }
-
-        private static void DefaultWarning(string message)
-        {
-            Console.WriteLine(Format("W: " + message));
-        }
-
-        private static void DefaultInfo(string message)
-        {
-            Console.WriteLine(Format("I: " + message));
-        }
-
-        private static void DefaultVerbose(string message)
-        {
-            Console.WriteLine(Format("V: " + message));
-        }
-
-        private static void DefaultDebug(string message)
-        {
-            Console.WriteLine(Format("D: " + message));
-        }
+#pragma warning disable CA2211 // Non-constant fields should not be visible
+        public static DelegateLog Error     = (message) => Console.WriteLine(Format("E: " + message));
+        public static DelegateLog Warning   = (message) => Console.WriteLine(Format("W: " + message));
+        public static DelegateLog Info      = (message) => Console.WriteLine(Format("I: " + message));
+        public static DelegateLog Verbose   = (message) => Console.WriteLine(Format("V: " + message));
+        public static DelegateLog Debug     = (message) => Console.WriteLine(Format("D: " + message));
+#pragma warning restore CA2211 // Non-constant fields should not be visible
 
         private static string Format(string message)
         {
             var now = DateTime.Now;
-            return $"{now.Year.ToString("0000")}-{now.Month.ToString("00")}-{now.Day.ToString("00")} " +
-            $"{now.Hour.ToString("00")}:{now.Minute.ToString("00")}:{now.Second.ToString("00")} | {message}";
+            return $"{now.Year:0000}-{now.Month:00}-{now.Day:00} {now.Hour:00}:{now.Minute:00}:{now.Second:00} | {message}";
         }
     }
 }
